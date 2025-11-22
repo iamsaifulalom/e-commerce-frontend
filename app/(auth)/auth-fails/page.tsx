965a6ada6a831078681b859528729fa3a9017e46
Saiful Alom <saifulalom49590@gmail.com>
2025-11-23 01:36:07 +0600

    fix(auth): auth succes and fials comented for deplyment essues

--- app/(auth)/auth-fails/page.tsx
+++ app/(auth)/auth-fails/page.tsx
@@ -1,42 +1,42 @@
-"use client";
+// "use client";
 
-import { useSearchParams, useRouter } from "next/navigation";
-import { Suspense, useEffect } from "react";
-import { Loader2 } from "lucide-react";
+// import { useSearchParams, useRouter } from "next/navigation";
+// import { Suspense, useEffect } from "react";
+// import { Loader2 } from "lucide-react";
 
-export default function AuthSuccessPage() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
+// export default function AuthSuccessPage() {
+//   const searchParams = useSearchParams();
+//   const router = useRouter();
 
-  const token = searchParams.get("token"); // extract value outside useEffect
+//   const token = searchParams.get("token"); // extract value outside useEffect
 
-  useEffect(() => {
-    if (token) {
-      localStorage.setItem("authToken", token);
+//   useEffect(() => {
+//     if (token) {
+//       localStorage.setItem("authToken", token);
 
-      // Redirect after 500ms for smooth UX
-      setTimeout(() => router.push("/"), 500);
-    }
-  }, [router, token]); // ✅ include only primitive token & router
+//       // Redirect after 500ms for smooth UX
+//       setTimeout(() => router.push("/"), 500);
+//     }
+//   }, [router, token]); // ✅ include only primitive token & router
 
 
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
+//   return (
+//     <Suspense fallback={<div>Loading...</div>}>
 
-      <div className="min-h-screen flex items-center justify-center bg-background px-4">
-        <div className="flex flex-col items-center text-center">
-          <Loader2 className="h-10 w-10 animate-spin text-primary" />
+//       <div className="min-h-screen flex items-center justify-center bg-background px-4">
+//         <div className="flex flex-col items-center text-center">
+//           <Loader2 className="h-10 w-10 animate-spin text-primary" />
 
-          <h1 className="text-xl font-semibold text-foreground">
-            Signing you in…
-          </h1>
+//           <h1 className="text-xl font-semibold text-foreground">
+//             Signing you in…
+//           </h1>
 
-          <p className="text-muted-foreground text-sm">
-            Please wait while we securely set up your account.
-          </p>
-        </div>
-      </div>
-    </Suspense>
+//           <p className="text-muted-foreground text-sm">
+//             Please wait while we securely set up your account.
+//           </p>
+//         </div>
+//       </div>
+//     </Suspense>
 
-  );
-}
+//   );
+// }
