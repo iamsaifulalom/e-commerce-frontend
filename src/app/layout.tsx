1e4eac41290027234762bbf65028880427c14791
Saiful Alom <saifulalom49590@gmail.com>
2025-12-04 14:52:23 +0600

    feat(theme):add  darkmode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Bodoni_Moda, Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/provider/theme-provider";
 
 export const bodoniModa = Bodoni_Moda({
   variable: "--font-bodoni-moda",
@@ -24,9 +25,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${bodoniModa.variable} ${inter.variable}`}>
+    <html lang="en" suppressHydrationWarning className={`${bodoniModa.variable} ${inter.variable}`}>
       <body>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/layout/admin-header.tsx
+++ src/components/layout/admin-header.tsx
@@ -16,31 +16,37 @@ import {
     AvatarImage
 } from "../ui/avatar";
 import { NavUser } from "../ui/NavUser";
+import { ModeToggle } from "../ui/mode-toogle";
 
 export default function AdminHeader() {
     const { toggleSidebar } = useSidebar();
     return (
         <div className='w-full h-16 p-6 flex justify-between items-center sticky top-0 left-0 bg-muted'>
-            <TextAlignStart onClick={toggleSidebar} className="cursor-pointer xl:hidden" />
-            Admin Header
 
-            <DropdownMenu >
-                <DropdownMenuTrigger>
-                    <Avatar>
-                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                        <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-52 mr-4">
-                    <DropdownMenuItem>
-                        <NavUser email="test@gmail.com" name="Saiful alom" avatar="https://github.com/shadcn.png" />
-                    </DropdownMenuItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>Billing</DropdownMenuItem>
-                    <DropdownMenuItem>Team</DropdownMenuItem>
-                    <DropdownMenuItem variant="destructive">Sign out</DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
+            {/* side bar toogle icon */}
+            <TextAlignStart onClick={toggleSidebar} className="cursor-pointer xl:hidden" />
+            app logi
+
+            <div className="flex gap-3 items-center">
+                <ModeToggle />
+                <DropdownMenu >
+                    <DropdownMenuTrigger>
+                        <Avatar className="outline-none">
+                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent className="w-52 mr-4">
+                        <DropdownMenuItem>
+                            <NavUser email="test@gmail.com" name="Saiful alom" avatar="https://github.com/shadcn.png" />
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Billing</DropdownMenuItem>
+                        <DropdownMenuItem>Team</DropdownMenuItem>
+                        <DropdownMenuItem variant="destructive">Sign out</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
         </div>
     )
 }
